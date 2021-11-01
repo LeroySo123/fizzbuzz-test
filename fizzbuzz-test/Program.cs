0a3eba6c50c723a12dd79068289a2bd65738c6fe
Leroy so <tdvgoe@hotmail.com>
2021-11-01 16:54:06 +0000

    new

---
+++ fizzbuzz-test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace fizzbuzz_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+
+                if (fizz)
+                {
+                    if (buzz)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    Console.WriteLine("Fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                { 
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
